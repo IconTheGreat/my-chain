414b42154dccd5768ac88f97e714ebdd4f5dcaa0
ICON <iconthegreat01@gmail.com>
2025-10-15 12:13:14 +0100

    new changes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 mod block;
 mod blockchain;
+mod wallet;
+mod transaction;
 
 fn main() {
     let mut chain = blockchain::Blockchain::new();
-    chain.add_block("New Block".into());
-    chain.add_block("Another Block".into());
+    let tx1 = transaction::Transaction::new("address1".into(), 50);
+    let tx2 = transaction::Transaction::new("address2".into(), 75);
+    chain.add_block(vec![tx1, tx2]);
     println!("{:?}", chain);
     println!("{:?}", chain.get_latest_block());
+    let wallet = wallet::Wallet::new();
+    println!("Wallet Address: {:?}", wallet.get_address());
+    println!("New Wallet: {:?}", wallet);
+    let message = "Hello, blockchain!".as_bytes();
+    let signature = wallet.sign(message);
+    println!("Signature: {:?}", signature);
 }
